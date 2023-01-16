d4f4a150979300ecf6b346746ab5ef3f56e88e6c
lucija0905 <lm20190223@student.fon.bg.ac.rs>
2023-01-16 17:30:24 +0100

    Funkcionalna komponenta saloni, prikazani saloni iz baze u tabeli

--- reactapp/src/components/Saloni.js
+++ reactapp/src/components/Saloni.js
@@ -1,8 +1,46 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 function Saloni(props) {
+
+    const [sviSaloni, setSviSaloni] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/salon').then(odgovor => {
+            setSviSaloni(odgovor.data.data);
+            alert(props.uspeh)
+        }).catch(error => {
+            alert(props.neuspeh + error);
+        });
+    }, []);
+
     return (
         <div>
-
-        </div>
+            <table className="table table-bordered table-hover" id="tbl-sln">
+                <thead >
+                    <tr className="table-primary">
+                        <th >Naziv</th>
+                        <th>Vlasnik</th>
+                        <th>Adresa</th>
+                        <th>Grad</th>
+                        <th>Website</th>
+                    </tr>
+                </thead>
+                <tbody className="table-active">
+                    {sviSaloni.map(salon => {
+                        return (
+                            <tr key={salon.ID}>
+                                <td>{salon.Naziv}</td>
+                                <td>{salon.Vlasnik}</td>
+                                <td>{salon.Adresa}</td>
+                                <td>{salon.Grad}</td>
+                                <td>{salon.Website}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div >
     )
 }
 
